3eec3b49f6dd3d3ce9181d7aa3f15a20e720a4f6
Paul Ogolla <paulotieno2@gmail.com>
2021-08-18 15:08:46 +0300

    build navbar, add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import Head from 'next/head';
+import { Navbar } from '../components/Navbar';
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>Paul Ogolla</title>
+        <link rel='icon' href='/favicon-32x32.png' />
       </Head>
+      <Navbar />
       <div>Hello World</div>
     </div>
   );

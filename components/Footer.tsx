460091ba78a40a14278733b9ae24f430ca7af85a
Paul Ogolla <paulotieno2@gmail.com>
2021-08-20 08:34:35 +0300

    fix footer link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,35 +1,50 @@
+import Link from "next/link";
+
 export const Footer = () => {
   const date = new Date().getFullYear();
 
-  return(
-  <footer className="bg-black h-52 py-20 px-4"> 
-    <div className="text-center">
-      <ul className="inline-flex">
-        <li>
-          <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-3xl items-center justify-center hover:text-black" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/paulogolla/"><i
-              className="fab fa-linkedin"></i></a>
-        </li>
-        <li>
-          <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-3xl items-center justify-center hover:text-black" target="_blank" rel="noopener noreferrer" href="https://twitter.com/_paulzay_"><i
-              className="fab fa-twitter"></i></a>
-        </li>
-        <li>
-          <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-3xl items-center justify-center hover:text-black" target="_blank" rel="noopener noreferrer" href="https://github.com/paulzay/"><i
-              className="fab fa-github"></i></a>
-        </li>
-        <li>
-          <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-3xl items-center justify-center hover:text-black" target="_blank" rel="noopener noreferrer" href="https://paulzay.medium.com/">
-            <i className="fab fa-medium"></i>
-          </a>
-        </li>
-        <li>
-          <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-3xl items-center justify-center hover:text-black" target="_blank" rel="noopener noreferrer" href="https://angel.co/u/paul-ogolla">
-            <i className="fab fa-angellist " aria-hidden="true"></i>
-          </a>
-        </li>
-      </ul>
-    </div>
-    <p className="mt-10 mb-0 text-white text-center tracking-wide">&copy; Paul Ogolla <span>{date}</span>. All rights reserved.</p>
-  </footer>
+  return (
+    <footer className="bg-black h-52 py-20 px-4">
+      <div className="text-center">
+        <ul className="inline-flex">
+          <li>
+            <Link href="https://www.linkedin.com/in/paulogolla/">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-3xl items-center justify-center hover:text-black" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-linkedin"></i>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://twitter.com/_paulzay_">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-3xl items-center justify-center hover:text-black" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-twitter"></i>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://github.com/paulzay/">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-3xl items-center justify-center hover:text-black" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-github"></i>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://paulzay.medium.com/">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-3xl items-center justify-center hover:text-black" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-medium"></i>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://angel.co/u/paul-ogolla">
+              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white text-3xl items-center justify-center hover:text-black" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-angellist " aria-hidden="true"></i>
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <p className="mt-10 mb-0 text-white text-center tracking-wide">&copy; Paul Ogolla <span>{date}</span>. All rights reserved.</p>
+    </footer>
   )
 };

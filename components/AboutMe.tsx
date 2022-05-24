7eb7216aae6d40cd0de109c1fda66ff9922c510c
Paul Ogolla <paulotieno2@gmail.com>
2022-05-24 16:29:45 +0300

    add justify-text

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -4,22 +4,22 @@ export const AboutMe = () => {
       <div className="" data-aos="fade-up">
         <h2 className="mb-10 text-center">ABOUT ME</h2>
         <div className="left-text text-left px-10">
-          <p className="text-xl">
+          <p className="text-xl text-justify">
             I am a software developer with professional experience innovating
             solutions. I specialize in building applications specific to the
             business needs of my clients. I work in full-stack web development, mobile app development, and database
             management.
           </p>
-          <p className="text-xl">
+          <p className="text-xl text-justify">
             I have worked with a
             wide range of technologies including HTML/CSS/SCSS, Bootstrap, MaterialUI, TailwindCSS, JavaScript, TypeScript, Reactjs, Nextjs, Node & Express, MongoDB, PostgreSQL, Ruby & Ruby on Rails,
             REST API design, CircleCI, 3rd party APIs (Stripe, Google Auth, etc.), Dart, Flutter, Kotlin and
             many more.
           </p>
-          <p className="text-xl">
+          <p className="text-xl text-justify">
             Currently, I'm exploring the web3 space with Solidity.
           </p>
-          <p className="text-xl">
+          <p className="text-xl text-justify">
             When I'm not fixing bugs, I love to deejay, dance, cycle, skate and of course doomscroll on the internet.
           </p>
         </div>

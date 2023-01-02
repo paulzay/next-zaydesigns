727f37c7e0bf712b7fe0a82b11f749c1f5c69aff
Paul Ogolla <paulotieno2@gmail.com>
2023-01-02 14:56:11 +0300

    update about

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -2,7 +2,7 @@ export const AboutMe = () => {
   return (
     <div className="aboutme">
       <div className="" data-aos="fade-up">
-        <h2 className="mb-10 text-center">ABOUT ME</h2>
+        <h2 className="text-center">ABOUT ME</h2>
         <div className="left-text text-left px-10">
           <p className="text-xl text-justify">
             I am a software developer with professional experience innovating
@@ -15,12 +15,6 @@ export const AboutMe = () => {
             wide range of technologies including HTML/CSS/SCSS, Bootstrap, MaterialUI, TailwindCSS, JavaScript, TypeScript, Reactjs, Nextjs, Node & Express, MongoDB, PostgreSQL, Ruby & Ruby on Rails,
             REST API design, CircleCI, 3rd party APIs (Stripe, Google Auth, etc.), Dart, Flutter, Kotlin and
             many more.
-          </p>
-          <p className="text-xl text-justify">
-            Currently, I'm exploring the web3 space with Solidity.
-          </p>
-          <p className="text-xl text-justify">
-            When I'm not fixing bugs, I love to deejay, dance, cycle, skate and of course doomscroll on the internet.
           </p>
         </div>
       </div>

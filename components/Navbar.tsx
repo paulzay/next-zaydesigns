7a6223d9f354fd748509d5429abe41ae292475fd
Paul Ogolla <paulotieno2@gmail.com>
2023-01-02 14:55:39 +0300

    center nav links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -47,8 +47,8 @@ export const Navbar = () => {
         </button>
         <div className={`${active ? '' : 'hidden'
           }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}>
-          <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-end  flex flex-col lg:h-auto '>
-            <Link ctiveClass="active" to="home">
+          <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-center flex flex-col lg:h-auto'>
+            <Link activeClass="active" to="home">
               <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-medium items-center justify-center hover:bg-black hover:text-white '>
                 HOME
               </a>

a883d394e1ea5cfa696f7948c80008e525d47c85
Paul Ogolla <paulotieno2@gmail.com>
2023-01-02 14:56:28 +0300

    update projects

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -4,10 +4,11 @@ function ProjectSection(props: Props) {
   const { } = props
 
   return (
-    <div id="portfolio" className="portfolio ">
+    <div id="portfolio" className="portfolio">
       <div className="section-intro px-10">
         <div className="portfolio-header">
           <h2>PORTFOLIO</h2>
+          <p>Coming Soon</p>
         </div>
       </div>
     </div>

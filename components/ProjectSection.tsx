6c812b1e2eada7d752f65b4b84ae2ea4ded9d8d5
Paul Ogolla <paulotieno2@gmail.com>
2023-01-23 13:06:03 +0300

    add projects

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -51,9 +51,9 @@ function ProjectSection(props: Props) {
                 </ul>
               </div>
             </div>
-            <div className="flip">
+            {/* <div className="flip">
               <div className="front front2">
-                {/* <h1 className="text-shadow">Space Shooter</h1> */}
+                <h1 className="text-shadow">Space Shooter</h1>
               </div>
               <div className="back">
                 <h2>Space Shooter</h2>
@@ -63,14 +63,14 @@ function ProjectSection(props: Props) {
                     <a href="https://github.com/paulzay/shooter" target="_blank" rel="noopener noreferrer">
                       <i className="fab fa-github"></i></a>
                   </li>
-                  {/* <li>
+                  <li>
                     <a href="https://kitohealth.org/" target="_blank" rel="noopener noreferrer">
                       Demo
                     </a>
-                  </li> */}
+                  </li>
                 </ul>
               </div>
-            </div>
+            </div> */}
             <div className="flip">
               <div className="front front3">
                 {/* <h1 className="text-shadow">Reels</h1> */}
@@ -86,6 +86,66 @@ function ProjectSection(props: Props) {
                   <li>
                     <a href="https://reactreel.netlify.app/" target="_blank" rel="noopener noreferrer">
                       Demo
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flip">
+              <div className="front front2">
+                {/* <h1 className="text-shadow">Reels</h1> */}
+              </div>
+              <div className="back">
+                <h2>Rolex Digital</h2>
+                <p>Design for digital marketing firm</p>
+                <ul>
+                  <li>
+                    <a href="https://github.com/paulzay/reels" target="_blank" rel="noopener noreferrer">
+                      <i className="fab fa-github"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://www.figma.com/proto/d9hrzosFpYTaCXRwE84DgL/Landing-page-design-(Copy)?node-id=65%3A126&scaling=min-zoom&page-id=0%3A1&hide-ui=1" target="_blank" rel="noopener noreferrer">
+                      Figma Prototype
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flip">
+              <div className="front front5">
+                {/* <h1 className="text-shadow">Reels</h1> */}
+              </div>
+              <div className="back">
+                <h2>Yam Yam</h2>
+                <p>A design project for recipes</p>
+                <ul>
+                  <li>
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                      <i className="fab fa-github"></i></a>
+                  </li>
+                  <li>
+                    <a href="hhttps://www.figma.com/proto/sxwjz7bXbOitWdXHGOg87z/recipe-app-design-file?node-id=35%3A385&scaling=scale-down&page-id=7%3A412&starting-point-node-id=35%3A210&show-proto-sidebar=1" target="_blank" rel="noopener noreferrer">
+                      Figma Prototype
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flip">
+              <div className="front front6">
+                {/* <h1 className="text-shadow">Reels</h1> */}
+              </div>
+              <div className="back">
+                <h2>Projetti</h2>
+                <p>A design project that enables teams collaboration and project management</p>
+                <ul>
+                  <li>
+                    <a href="#" target="_blank" rel="noopener noreferrer">
+                      <i className="fab fa-github"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://www.figma.com/proto/rcjYAJFTH1vYc4BXl0iHTe/Projetti--A-SaaS-webpages-(Copy)?node-id=19%3A25&scaling=scale-down&page-id=19%3A24&starting-point-node-id=70%3A1038&show-proto-sidebar=1/" target="_blank" rel="noopener noreferrer">
+                      Figma Prototype
                     </a>
                   </li>
                 </ul>
